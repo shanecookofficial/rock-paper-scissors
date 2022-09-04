20a519a15ba1e414143a72e2e06f8fe094213144
Shane Cook <shanecook@Shanes-MacBook-Pro.local>
2022-09-04 16:49:01 -0400

    Added a new function
    getComputerInput calls the setComputerInput function and returns that function.

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
-function generateComputerInput() {
+function setComputerInput() {
     let choices = ['rock','paper','scissors'];
     let choice = Math.floor(Math.random() * 3);
     return choices[choice];
+}
+
+function getComputerInput() {
+    return setComputerInput();
 }
