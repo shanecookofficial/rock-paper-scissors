44c5ee0fc31a3119e7fe5375f127ff3c945d9e28
Shane Cook <shanecook@Shanes-MacBook-Pro.local>
2022-09-05 14:34:47 -0400

    Added message result area on HTML and gameLogic function on js

--- game.js
+++ game.js
@@ -3,3 +3,23 @@ function generateComputerInput() {
     let choice = Math.floor(Math.random() * 3);
     return choices[choice];
 }
+
+function gameLogic(userInput, computerInput) {
+    if (userInput == computerInput) {
+        return 'tie';
+    } else if (userInput == 'rock' && computerInput == 'paper') {
+        return 'computer';
+    } else if (userInput == 'rock' && computerInput == 'scissors') {
+        return 'user';
+    } else if (userInput == 'paper' && computerInput == 'rock') {
+        return 'user';
+    } else if (userInput == 'paper' && computerInput == 'scissors') {
+        return 'computer';
+    } else if (userInput == 'scissors' && computerInput == 'paper') {
+        return 'user';
+    } else if (userInput == 'scissors' && computerInput == 'rock') {
+        return 'computer';
+    } else {
+        console.log('Something is wrong :/')
+    }
+}
