c2d9a340060f89be8ff1f7a888b3db969ce76747
Shane Cook <shanecook@Shanes-MacBook-Pro.local>
2022-09-05 13:39:39 -0400

    removed getComputerInput function
    The extra function was not needed.

--- game.js
+++ game.js
@@ -1,9 +1,5 @@
-function setComputerInput() {
+function generateComputerInput() {
     let choices = ['rock','paper','scissors'];
     let choice = Math.floor(Math.random() * 3);
     return choices[choice];
-}
-
-function getComputerInput() {
-    return setComputerInput();
 }
